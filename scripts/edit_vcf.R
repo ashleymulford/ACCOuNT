b533796ee3a8768bebbd0058805c653e6b2150f3
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-09-01 14:45:19 -0400

    Create edit_vcf.R

---
+++ scripts/edit_vcf.R
@@ -0,0 +1,13 @@
+library(vcfR)
+
+#Create function to paste
+"%&%" = function(a,b) paste(a,b,sep="")
+
+for (chr in 1:22) {
+  vcf <- read.vcfR("account_blacks_chr" %&% chr %&% ".vcf")
+  k<-paste("chr", vcf$V1[1], sep = "")
+  lng<-length(vcf$V1)
+  col<-rep(k, lng)
+  vcf$V1<-col
+  write.vcf(vcf, "test_account_blacks_chr" %&% chr %&% ".vcf", sep = "\t", quote = F, col.names = F)
+}
