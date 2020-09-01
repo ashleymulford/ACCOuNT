49a4cce522d6c7a409cf2d683719fe8b4ad7dbb5
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-09-01 15:48:11 -0400

    Update edit_vcf.R

--- scripts/edit_vcf.R
+++ scripts/edit_vcf.R
@@ -3,6 +3,11 @@ library(vcfR)
 #Create function to paste
 "%&%" = function(a,b) paste(a,b,sep="")
 
+chr<-22
+vcf <- read.vcfR("vcfs/account_blacks_chr" %&% chr %&% ".vcf")
+vcf@fix[chr]
+main<-vcf@fix
+
 for (chr in 1:22) {
   vcf <- read.vcfR("account_blacks_chr" %&% chr %&% ".vcf")
   k<-paste("chr", vcf$V1[1], sep = "")
