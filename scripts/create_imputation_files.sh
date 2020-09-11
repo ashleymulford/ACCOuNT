550697a5da43cbca174cdb35db1b995511574bf1
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-09-11 10:52:02 -0400

    Update create_imputation_files.sh

--- scripts/create_imputation_files.sh
+++ scripts/create_imputation_files.sh
@@ -3,11 +3,13 @@ for chr in {1..22};
 do plink --bfile /home/wheelerlab3/Data/ACCOuNT/ACCOuNT_PLINKfiles/ACCOuNTBLACKSfinal --chr ${chr} --recode vcf --out account_blacks_chr${chr};
 done
 
-#GRCh38 requires chromosomes as "chr1" not "1" - convert with Rscript: 
-Rscript edit_vcf.R
+#GRCh38 requires chromosomes as "chr1" not "1" - convert with awk: 
+for chr in {1..22};
+do awk '{if($0 !~ /^#/) print "chr"$0; else print $0}' account_blacks_chr${chr}.vcf > account_blacks_chrchr${chr}.vcf;
+done
 
 #bgzip files:
 for chr in {1..22};
-do bgzip account_blacks_chr${chr}.vcf;
+do bgzip account_blacks_chrchr${chr}.vcf;
 done
 
