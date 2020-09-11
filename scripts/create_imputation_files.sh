dec2cef0b55abaf6bdaae7d0da7fe53aac565651
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-09-11 12:30:58 -0400

    Update create_imputation_files.sh

--- scripts/create_imputation_files.sh
+++ scripts/create_imputation_files.sh
@@ -13,3 +13,21 @@ for chr in {1..22};
 do bgzip account_blacks_chrchr${chr}.vcf;
 done
 
+#Imputation successful for chrs 21,22
+
+#Imputation failed for chrs 19,20 - due to the following error:
+  #Error: More than 10000 obvious strand flips have been detected. Please check strand. Imputation cannot be started!
+
+
+#once imputation is complete download and renmae files:
+wget https://imputation.biodatacatalyst.nhlbi.nih.gov/share/results/138cfce60db2f4da992af59349a8dfa1/chr_22.zip
+
+##problem unzipping - unsupported compression method 99 (refers to Adavanced Encryption Standard encryption)
+  ## need another software like 7zip to unzip imputted files
+for chr in {1..22};
+do unzip chr_${chr}.zip;
+done
+
+for chr in {1..22};
+do chr_${chr}.zip > account_blacks_chr{chr}_imputed.vcf
+done
