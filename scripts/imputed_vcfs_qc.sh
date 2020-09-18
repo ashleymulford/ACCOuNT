15c5fdc0320312478eca3b322eea83be93cc0338
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-09-18 12:18:58 -0400

    Update imputed_vcfs_qc.sh

--- scripts/imputed_vcfs_qc.sh
+++ scripts/imputed_vcfs_qc.sh
@@ -1,6 +1,12 @@
-#Filter out SNPs with maf < 0.01
+#For both black and white cohorts:
 
+#Filter out SNPs with maf < 0.01:
 for chr in {1..22};
 do bcftools view -q 0.01:minor chr${chr}.dose.vcf.gz | bgzip -c > chr${chr}_maf01.vcf.gz;
+done
+
+#Filer out SNPs with r^2 < 0.8:
+for chr in {1..21};
+do 
 done
 
