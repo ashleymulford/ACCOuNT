d5132920a971e9f4ff797a6f200292ecf5cf79a2
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-09-30 16:06:44 -0400

    Update imputed_vcfs_to_pred_dosages.sh

--- scripts/imputed_vcfs_to_pred_dosages.sh
+++ scripts/imputed_vcfs_to_pred_dosages.sh
@@ -1,6 +1,9 @@
 
-#Convert vcf to ped/map
+#Convert vcf to ped/map, bed/bim/fam, frq
 
 for chr in {1..22};
 do plink --vcf chr${chr}_maf01_r28.vcf.gz --recode --out chr${chr}_maf01_r28;
+plink --file chr${chr}_maf01_r28 --make-bed --out chr${chr}_maf01_r28;
+plink --file chr${chr}_maf01_r28 -freq --out chr${chr}_maf01_r28;
 done
+
