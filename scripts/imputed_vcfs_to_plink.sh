13d688f9cf073c2c0434782cde96bc95cc7fd8df
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-09-30 17:27:01 -0400

    Update imputed_vcfs_to_plink.sh

--- scripts/imputed_vcfs_to_plink.sh
+++ scripts/imputed_vcfs_to_plink.sh
@@ -11,10 +11,13 @@ done
 awk '{print $1, $2}' chr1_maf01_r28.fam > samples_blacks.txt
 
 
-#Starting doasges for predixcan (chr, varid, pos, a1, a2 - need to add maf & id1-idn):
+#Starting dosage files for predixcan (chr, varid, pos, a1, a2 - need to add maf & id1-idn):
 ## col 6 has A1 (ref) & col 5 has A2 (alt/minor)
 for chr in {1..22};
 do awk '{print $1, $2, $4, $6, $5}' chr${chr}_maf01_r28.bim > chr${chr}_dosage.txt;
 done
 
+
+#get maf from vcfs
+#get dosages (counts) from ped file 
 
