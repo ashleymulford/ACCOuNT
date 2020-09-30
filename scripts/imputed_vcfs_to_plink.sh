465afcf60d3aed637fa985905848c8e5d76d026e
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-09-30 17:24:13 -0400

    Update imputed_vcfs_to_plink.sh

--- scripts/imputed_vcfs_to_plink.sh
+++ scripts/imputed_vcfs_to_plink.sh
@@ -1,9 +1,20 @@
+#Do for both pops:
 
-#Convert vcf to ped/map, bed/bim/fam, frq
+#Convert vcf to ped/map, bed/bim/fam
 
 for chr in {1..22};
 do plink --vcf chr${chr}_maf01_r28.vcf.gz --recode --out chr${chr}_maf01_r28;
 plink --file chr${chr}_maf01_r28 --make-bed --out chr${chr}_maf01_r28;
-plink --file chr${chr}_maf01_r28 -freq --out chr${chr}_maf01_r28;
 done
+
+#Samples file for predixcan:
+awk '{print $1, $2}' chr1_maf01_r28.fam > samples_blacks.txt
+
+
+#Starting doasges for predixcan (chr, varid, pos, a1, a2 - need to add maf & id1-idn):
+## col 6 has A1 (ref) & col 5 has A2 (alt/minor)
+for chr in {1..22};
+do awk '{print $1, $2, $4, $6, $5}' chr${chr}_maf01_r28.bim > chr${chr}_dosage.txt;
+done
+
 
