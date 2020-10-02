e46bd2f8416063e11132bc80a7e10f6f02c261e4
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-10-02 14:03:11 -0400

    Update and rename imputed_vcfs_to_plink.sh to make_samples_imputed_vcfs_to_plink.sh

--- scripts/make_samples_imputed_vcfs_to_plink.sh
+++ scripts/make_samples_imputed_vcfs_to_plink.sh
@@ -11,13 +11,3 @@ done
 awk '{print $1, $2}' chr1_maf01_r28.fam > samples_blacks.txt
 
 
-#Starting dosage files for predixcan (chr, varid, pos, a1, a2 - need to add maf & id1-idn):
-## col 6 has A1 (ref) & col 5 has A2 (alt/minor)
-for chr in {1..22};
-do awk '{print $1, $2, $4, $6, $5}' chr${chr}_maf01_r28.bim > chr${chr}_dosage.txt;
-done
-
-
-#get maf from vcfs
-#get dosages (counts) from ped files 
-
