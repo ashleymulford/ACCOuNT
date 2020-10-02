dd050b3dbce40d0ce3d6d93f8abcbd0368ac0660
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-10-02 13:32:43 -0400

    Update vcf_to_dosage.py

--- scripts/vcf_to_dosage.py
+++ scripts/vcf_to_dosage.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 '''This python script takes one imputed vcf file as input, 
-removes SNPs with R2<0.8 and MAF>0.01 (options to change),
-and makes output files:
-chr*.dosage.txt.gz
-samples.txt
+and makes a predixcan dosage fornatted output files:
+chr*.dosage.txt
 dose allele is Allele2, see https://github.com/hakyimlab/PrediXcan/blob/master/Software/HOWTO-beta.md'''
 
 import gzip
@@ -43,25 +41,29 @@ mafthresh = args.maf
 r2thresh = args.rsq
 chrfile = chrpath + "chr" + c + "_maf01_r28.vcf.gz"
 
-
 # get dosage file data
 if(os.path.exists(chrpath + 'UMich_dosages/') == False):
     os.mkdir(chrpath + 'UMich_dosages/')
 
-outdosage = gzip.open(chrpath + "UMich_dosages/chr" + c + ".maf" + str(mafthresh) + ".r2" + str(r2thresh) + ".dosage.txt.gz","wb")
+outdosage = open(chrpath + "UMich_dosages/chr" + c + ".maf" + str(mafthresh) + ".r2" + str(r2thresh) + ".dosage.txt","w")
 for line in gzip.open(chrfile):
     if(line.startswith(b'##')):
         continue
-    if(line.startswith(b'#CHROM')): #only one line should match #CHROM
-	continue
+    if(line.startswith(b'#CHROM')):
+        continue
     arr = line.strip().split()
     (chr, pos, id, ref, alt, qual, filter, info, format) = arr[0:9]
     gt_dosagerow = arr[9:]
     #see http://www.python-course.eu/lambda.php for details
-    dosagerow = map(lambda x : float(x.split(":")[1]), gt_dosagerow) #lambda function to split each info entry and collect the dosage
-    freqalt = round(sum(dosagerow)/(len(dosagerow)*2),4) #calc ALT allele freq (I found that ALT is not always the minor allele)
+    dosagerow = map(lambda x : float(x.split(b":")[1]), gt_dosagerow) #lambda function to split each info entry and collect the dosage
+    info_list = info.split(b";")
+    af_list = info_list[0].split(b"=")
+    freqalt = af_list[1]
     dosages = ' '.join(map(str,dosagerow))
-    output = 'chr' + chr + ' ' + rsid + ' ' + pos + ' ' + ref + ' ' + alt + ' ' + str(freqalt) + ' ' + dosages + '\n'
+    output = str(chr) + ' ' + str(id) + ' ' + str(pos) + ' ' + str(ref) + ' ' + str(alt) + ' ' + str(freqalt) + ' ' + dosages + '\n'
+    output = output.replace("b", "")
+    output = output.replace("'", "")
     outdosage.write(output)
 
 outdosage.close()
+
