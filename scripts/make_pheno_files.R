885eb2c0c3351c75ef2815fb6419eb9a1748f725
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-12-01 16:50:37 -0500

    Add files via upload

---
+++ scripts/make_pheno_files.R
@@ -0,0 +1,19 @@
+library(data.table)
+library(dplyr)
+
+clop<-fread("account_clopidogrel_processed_matched_IDs_all.csv")
+noac<-fread("account_noac_processed_matched_IDs_all.csv")
+warf<-fread("account_warfarin_processed_matched_IDs_all.csv")
+
+clop_small<-clop[, c(1:9,28,212)]
+noac_small<-noac[, c(1:9,21,22,83)]
+#idk which pheno for warf, add later
+#warf_small<-warf[, c(1:9,22)]
+
+fwrite(clop_small, "clop_pheno_with_ids.txt", sep = "\t", quote = F, na = NA)
+fwrite(noac_small, "noac_pheno_with_ids.txt", sep = "\t", quote = F, na = NA)
+#fwrite(warf_small, "warf_pheno_with_ids.txt", sep = "\t", quote = F, na = NA)
+
+
+
+
