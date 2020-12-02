25dfd7cb00fd35bf79b32b709545be26b02e189f
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-12-02 16:04:57 -0500

    Update gwas_qc_seperate_ped_by_drug.R

--- scripts/gwas_qc_seperate_ped_by_drug.R
+++ scripts/gwas_qc_seperate_ped_by_drug.R
@@ -6,7 +6,7 @@ noac<-fread("noac_pheno_with_ids.txt")
 warf<-fread("account_warfarin_processed_matched_IDs_all.csv")
 
 clop_ids<-filter(clop, startsWith(clop$`Study ID (PharmGKB ID)`, "PA"))
-noac_ids<-filter(noac, startsWith(noac$`Study ID (PharmGKB ID)`, "PA"))
+noac_ids<-filter(noac, startsWith(noac$`Study ID (PharmGKB ID)`, "PA") | startsWith(noac$`Study ID (PharmGKB ID)`, "pa"))
 apix<-filter(noac_ids, noac_ids$`Which NOAC Drug Used` == "apixaban")
 riva<-filter(noac_ids, noac_ids$`Which NOAC Drug Used` == "rivaroxaban")
 warf_ids<-filter(warf, startsWith(warf$`Study ID (PharmGKB ID)`, "PA"))
