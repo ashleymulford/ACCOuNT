0690bf8ccae7503cad8627f82373b7bbc928af4a
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-12-02 16:32:22 -0500

    Update gwas_qc_ibd.sh

--- scripts/gwas_qc_ibd.sh
+++ scripts/gwas_qc_ibd.sh
@@ -13,12 +13,17 @@ do
 echo chr${i}_maf01_r28 >> mergelist.txt
 done
 
-plink --file chr1_maf01_r28 --merge-list mergelist.txt --make-bed --out black_imputed_filtered_allchr
+plink --file chr1_maf01_r28 --merge-list mergelist.txt --make-bed --out blacks_imputed_filtered_allchr
 
 #make ped/map:
-plink --bfile black_imputed_filtered_allchr --recode --out black_imputed_filtered_allchr
-
+plink --bfile blacks_imputed_filtered_allchr --recode --out blacks_imputed_filtered_allchr
 
 # Run Rscript to separate by drug: gwas_qc_separate_ped_by_drug.R
 #creates four ped files with individuals grouped by drug taken (same map file is fine, notes snp info not people)
+
+#make bed/bim/fam files for each drug:
+plink --ped blacks_clop.ped --map blacks_imputed_filtered_allchr.map --make-bed --out blacks_clop
+plink --ped blacks_apix.ped --map blacks_imputed_filtered_allchr.map --make-bed --out blacks_apix
+plink --ped blacks_riva.ped --map blacks_imputed_filtered_allchr.map --make-bed --out blacks_riva
+plink --ped blacks_warf.ped --map blacks_imputed_filtered_allchr.map --make-bed --out blacks_warf
 
