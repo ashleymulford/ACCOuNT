8bb09a60c6ee324cf8a839825d31f30a1d0509aa
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-12-02 16:46:35 -0500

    Update and rename gwas_qc_separate_ped_by_drug.R to get_drug_ids.R

---
+++ scripts/get_drug_ids.R
@@ -0,0 +1,22 @@
+library(data.table)
+library(dplyr)
+
+clop<-fread("/home/ashley/account/phenos/clop_pheno_with_ids.txt")
+noac<-fread("/home/ashley/account/phenos/noac_pheno_with_ids.txt")
+warf<-fread("/home/ashley/account/phenos/account_warfarin_processed_matched_IDs_all.csv")
+
+clop_ids<-filter(clop, startsWith(clop$`Study ID (PharmGKB ID)`, "PA"))
+apix<-filter(noac, noac$`Which NOAC Drug Used` == "apixaban")
+riva<-filter(noac, noac$`Which NOAC Drug Used` == "rivaroxaban")
+warf_ids<-filter(warf, startsWith(warf$`Study ID (PharmGKB ID)`, "PA"))
+
+clop_ids<-clop_ids[, c(1:2)]
+apix<-apix[, c(1:2)]
+riva<-riva[, c(1:2)]
+warf_ids<-warf_ids[, c(1:2)]
+
+fwrite(clop_ids, "/home/ashley/account/gwas_qc/blacks/clop_ids.txt", sep = "\t", quote = F, col.names = F)
+fwrite(apix, "/home/ashley/account/gwas_qc/blacks/apix_ids.txt", sep = "\t", quote = F, col.names = F)
+fwrite(riva, "/home/ashley/account/gwas_qc/blacks/riva_ids.txt", sep = "\t", quote = F, col.names = F)
+fwrite(warf_ids, "/home/ashley/account/gwas_qc/blacks/warf_ids.txt", sep = "\t", quote = F, col.names = F)
+
