1b27cab4a71b304eba58263e0b15ee6e66824b2c
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-12-02 16:23:00 -0500

    Update gwas_qc_seperate_ped_by_drug.R

--- scripts/gwas_qc_seperate_ped_by_drug.R
+++ scripts/gwas_qc_seperate_ped_by_drug.R
@@ -1,14 +1,30 @@
 library(data.table)
 library(dplyr)
 
-clop<-fread("clop_pheno_with_ids.txt")
-noac<-fread("noac_pheno_with_ids.txt")
-warf<-fread("account_warfarin_processed_matched_IDs_all.csv")
+clop<-fread("/home/ashley/account/phenos/clop_pheno_with_ids.txt")
+noac<-fread("/home/ashley/account/phenos/noac_pheno_with_ids.txt")
+warf<-fread("/home/ashley/account/phenos/account_warfarin_processed_matched_IDs_all.csv")
 
 clop_ids<-filter(clop, startsWith(clop$`Study ID (PharmGKB ID)`, "PA"))
-noac_ids<-filter(noac, startsWith(noac$`Study ID (PharmGKB ID)`, "PA") | startsWith(noac$`Study ID (PharmGKB ID)`, "pa"))
-apix<-filter(noac_ids, noac_ids$`Which NOAC Drug Used` == "apixaban")
-riva<-filter(noac_ids, noac_ids$`Which NOAC Drug Used` == "rivaroxaban")
+apix<-filter(noac, noac$`Which NOAC Drug Used` == "apixaban")
+riva<-filter(noac, noac$`Which NOAC Drug Used` == "rivaroxaban")
 warf_ids<-filter(warf, startsWith(warf$`Study ID (PharmGKB ID)`, "PA"))
+
+clop_ids<-clop_ids[, c(1:2)]
+apix<-apix[, c(1:2)]
+riva<-riva[, c(1:2)]
+warf_ids<-warf_ids[, c(1:2)]
+
+ped<-fread("/home/ashley/account/gwas_qc/blacks/blacks_imputed_filtered_allchr.ped", header = F)
+
+clop_ped<-left_join(clop_ids, ped, by = c("V1" = "V1", "V2" = "V2"))
+apix_ped<-left_join(apix, ped, by = c("V1" = "V1", "V2" = "V2"))
+riva_ped<-left_join(riva, ped, by = c("V1" = "V1", "V2" = "V2"))
+warf_ped<-left_join(warf_ids, ped, by = c("V1" = "V1", "V2" = "V2"))
+      
+fwrite(clop_ped, "/home/ashley/account/gwas_qc/blacks/blacks_clop.ped", sep = "\t", quote = F)
+fwrite(apix_ped, "/home/ashley/account/gwas_qc/blacks/blacks_apix.ped", sep = "\t", quote = F)
+fwrite(riva_ped, "/home/ashley/account/gwas_qc/blacks/blacks_riva.ped", sep = "\t", quote = F)
+fwrite(warf_ped, "/home/ashley/account/gwas_qc/blacks/blacks_warf.ped", sep = "\t", quote = F)
 
 
