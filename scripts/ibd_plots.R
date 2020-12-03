c5581282995cd18be33092e3d39ffce4b9d3b94d
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-12-03 14:35:53 -0500

    Create ibd_plots.R

---
+++ scripts/ibd_plots.R
@@ -0,0 +1,7 @@
+library(data.table)
+library(dplyr)
+
+clop_ibd<-fread("/home/ashley/account/gwas_qc/blacks/blacks_clop_ibd.genome")
+apix_ibd<-fread("/home/ashley/account/gwas_qc/blacks/blacks_apix_ibd.genome")
+riva_ibd<-fread("/home/ashley/account/gwas_qc/blacks/blacks_riva_ibd.genome")
+warf_ibd<-fread("/home/ashley/account/gwas_qc/blacks/blacks_warf_ibd.genome")
