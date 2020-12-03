433f4eac2c2115e2ec9ca113db7626253de0fb10
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-12-03 15:01:37 -0500

    Update ibd_plots.R

--- scripts/ibd_plots.R
+++ scripts/ibd_plots.R
@@ -5,3 +5,43 @@ clop_ibd<-fread("/home/ashley/account/gwas_qc/blacks/blacks_clop_ibd.genome")
 apix_ibd<-fread("/home/ashley/account/gwas_qc/blacks/blacks_apix_ibd.genome")
 riva_ibd<-fread("/home/ashley/account/gwas_qc/blacks/blacks_riva_ibd.genome")
 warf_ibd<-fread("/home/ashley/account/gwas_qc/blacks/blacks_warf_ibd.genome")
+
+#Clop IBD plot
+pdf("/home/ashley/account/gwas_qc/blacks/clop_ibd.pdf")
+ggplot(data=clop_ibd, aes(x=Z0, y=Z1)) +
+    geom_point(alpha=1/4) +
+    theme_bw() +
+    coord_cartesian(xlim =c(0, 1), ylim = c(0, 1)) +
+    theme(text = element_text(size = 12), plot.title = element_text(hjust = 0.5)) +
+    ggtitle("Blacks Clopidogrel IBD")
+dev.off()
+
+#Apix IBD plot
+pdf("/home/ashley/account/gwas_qc/blacks/apix_ibd.pdf")
+ggplot(data=apix_ibd, aes(x=Z0, y=Z1)) +
+    geom_point(alpha=1/4) +
+    theme_bw() +
+    coord_cartesian(xlim =c(0, 1), ylim = c(0, 1)) +
+    theme(text = element_text(size = 12), plot.title = element_text(hjust = 0.5)) +
+    ggtitle("Blacks Apixaban IBD")
+dev.off()
+
+#Riva IBD plot
+pdf("/home/ashley/account/gwas_qc/blacks/riva_ibd.pdf")
+ggplot(data=riva_ibd, aes(x=Z0, y=Z1)) +
+    geom_point(alpha=1/4) +
+    theme_bw() +
+    coord_cartesian(xlim =c(0, 1), ylim = c(0, 1)) +
+    theme(text = element_text(size = 12), plot.title = element_text(hjust = 0.5)) +
+    ggtitle("Blacks Rivaroxaban IBD")
+dev.off()
+
+#Warf IBD plot
+pdf("/home/ashley/account/gwas_qc/blacks/warf_ibd.pdf")
+ggplot(data=warf_ibd, aes(x=Z0, y=Z1)) +
+    geom_point(alpha=1/4) +
+    theme_bw() +
+    coord_cartesian(xlim =c(0, 1), ylim = c(0, 1)) +
+    theme(text = element_text(size = 12), plot.title = element_text(hjust = 0.5)) +
+    ggtitle("Blacks Warfarin IBD")
+dev.off()
