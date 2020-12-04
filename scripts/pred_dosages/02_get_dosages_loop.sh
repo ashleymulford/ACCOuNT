2a7b4cca8f8d06865c554385ecd403f190e721d1
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-12-04 15:29:52 -0500

    Update 02_get_dosages_loop.sh

--- scripts/pred_dosages/02_get_dosages_loop.sh
+++ scripts/pred_dosages/02_get_dosages_loop.sh
@@ -1,21 +1,21 @@
 #make dosages for each drug group:
 
 for chr in {1..22};
-do python /home/ashley/account/scripts/vcf_to_dosage.py -i /home/ashley/account/vcfs/blacks_imputed/clop/ -c ${chr} -d clop;
+do python /home/ashley/account/vcfs/blacks_imputed/vcf_to_dosage.py -i /home/ashley/account/vcfs/blacks_imputed/clop/ -c ${chr} -d clop;
 done
 
 for chr in {1..22};
-do python /home/ashley/account/scripts/vcf_to_dosage.py -i /home/ashley/account/vcfs/blacks_imputed/apix/ -c ${chr} -d apix;
+do python /home/ashley/account/vcfs/blacks_imputed/vcf_to_dosage.py -i /home/ashley/account/vcfs/blacks_imputed/apix/ -c ${chr} -d apix;
 done
 
 for chr in {1..22};
-do python /home/ashley/account/scripts/vcf_to_dosage.py -i /home/ashley/account/vcfs/blacks_imputed/riva/ -c ${chr} -d riva;
+do python /home/ashley/account/vcfs/blacks_imputed/vcf_to_dosage.py -i /home/ashley/account/vcfs/blacks_imputed/riva/ -c ${chr} -d riva;
 done
 
 for chr in {1..22};
-do python /home/ashley/account/scripts/vcf_to_dosage.py -i /home/ashley/account/vcfs/blacks_imputed/noac/ -c ${chr} -d noac;
+do python /home/ashley/account/vcfs/blacks_imputed/vcf_to_dosage.py -i /home/ashley/account/vcfs/blacks_imputed/noac/ -c ${chr} -d noac;
 done
 
 for chr in {1..22};
-do python /home/ashley/account/scripts/vcf_to_dosage.py -i /home/ashley/account/vcfs/blacks_imputed/warf/ -c ${chr} -d warf;
+do python /home/ashley/account/vcfs/blacks_imputed/vcf_to_dosage.py -i /home/ashley/account/vcfs/blacks_imputed/warf/ -c ${chr} -d warf;
 done
