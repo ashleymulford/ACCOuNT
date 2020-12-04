c2b30fc961072a9778d3a56ceeeb67bc95579cf9
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-12-04 15:15:12 -0500

    Update 02_get_dosages_loop.sh

--- scripts/pred_dosages/02_get_dosages_loop.sh
+++ scripts/pred_dosages/02_get_dosages_loop.sh
@@ -1,5 +1,21 @@
 #For both black and white cohorts:
 
 for chr in {1..22};
-do python /home/ashley/account/scripts/vcf_to_dosage.py -i /home/ashley/account/vcfs/blacks_imputed/ -c ${chr};
+do python /home/ashley/account/scripts/vcf_to_dosage.py -i /home/ashley/account/vcfs/blacks_imputed/clop/ -c ${chr};
+done
+
+for chr in {1..22};
+do python /home/ashley/account/scripts/vcf_to_dosage.py -i /home/ashley/account/vcfs/blacks_imputed/apix/ -c ${chr};
+done
+
+for chr in {1..22};
+do python /home/ashley/account/scripts/vcf_to_dosage.py -i /home/ashley/account/vcfs/blacks_imputed/riva/ -c ${chr};
+done
+
+for chr in {1..22};
+do python /home/ashley/account/scripts/vcf_to_dosage.py -i /home/ashley/account/vcfs/blacks_imputed/noac/ -c ${chr};
+done
+
+for chr in {1..22};
+do python /home/ashley/account/scripts/vcf_to_dosage.py -i /home/ashley/account/vcfs/blacks_imputed/warf/ -c ${chr};
 done
