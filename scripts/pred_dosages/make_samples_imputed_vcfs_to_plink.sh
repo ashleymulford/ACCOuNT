88d003259bce99ec273fea802b7cee916b7d8b82
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-12-04 16:05:35 -0500

    Update make_samples_imputed_vcfs_to_plink.sh

--- scripts/pred_dosages/make_samples_imputed_vcfs_to_plink.sh
+++ scripts/pred_dosages/make_samples_imputed_vcfs_to_plink.sh
@@ -11,3 +11,14 @@ done
 awk '{print $1, $2}' chr1_maf01_r28.fam > samples_blacks.txt
 
 
+#For Clop:
+
+for chr in {1..22};
+do plink --vcf clop_chr${chr}.recode.vcf.gz --recode --out clop_chr${chr};
+plink --file clop_chr${chr} --make-bed --out clop_chr${chr};
+done
+
+#Samples file for predixcan:
+awk '{print $1, $2}' clop_chr1.fam > samples_blacks_clop.txt
+
+
