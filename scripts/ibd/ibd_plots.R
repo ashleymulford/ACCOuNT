4943a3a410565476ebedb1c40de7b7061ecf0bf4
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-12-04 11:43:57 -0500

    Update ibd_plots.R

--- scripts/ibd/ibd_plots.R
+++ scripts/ibd/ibd_plots.R
@@ -2,6 +2,7 @@ library(data.table)
 library(dplyr)
 
 clop_ibd<-fread("/home/ashley/account/gwas_qc/blacks/blacks_clop_ibd.genome")
+noac_ibd<-fread("/home/ashley/account/gwas_qc/blacks/blacks_noac_ibd.genome")
 apix_ibd<-fread("/home/ashley/account/gwas_qc/blacks/blacks_apix_ibd.genome")
 riva_ibd<-fread("/home/ashley/account/gwas_qc/blacks/blacks_riva_ibd.genome")
 warf_ibd<-fread("/home/ashley/account/gwas_qc/blacks/blacks_warf_ibd.genome")
@@ -14,6 +15,16 @@ ggplot(data=clop_ibd, aes(x=Z0, y=Z1)) +
     coord_cartesian(xlim =c(0, 1), ylim = c(0, 1)) +
     theme(text = element_text(size = 12), plot.title = element_text(hjust = 0.5)) +
     ggtitle("Blacks Clopidogrel IBD")
+dev.off()
+
+#NOAC IBD plot
+pdf("/home/ashley/account/gwas_qc/blacks/noac_ibd.pdf")
+ggplot(data=noac_ibd, aes(x=Z0, y=Z1)) +
+    geom_point(alpha=1/4) +
+    theme_bw() +
+    coord_cartesian(xlim =c(0, 1), ylim = c(0, 1)) +
+    theme(text = element_text(size = 12), plot.title = element_text(hjust = 0.5)) +
+    ggtitle("Blacks NOAC IBD")
 dev.off()
 
 #Apix IBD plot
