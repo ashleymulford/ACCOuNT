2b2aaf247e021709407f5fa783a50ddb5a2947ac
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-12-04 13:46:34 -0500

    Update ibd_plots.R

--- scripts/ibd/ibd_plots.R
+++ scripts/ibd/ibd_plots.R
@@ -1,11 +1,23 @@
 library(data.table)
 library(dplyr)
 
+all_ibd<-fread("/home/ashley/account/gwas_qc/blacks/blacks_all_ibd.genome")
 clop_ibd<-fread("/home/ashley/account/gwas_qc/blacks/blacks_clop_ibd.genome")
 noac_ibd<-fread("/home/ashley/account/gwas_qc/blacks/blacks_noac_ibd.genome")
 apix_ibd<-fread("/home/ashley/account/gwas_qc/blacks/blacks_apix_ibd.genome")
 riva_ibd<-fread("/home/ashley/account/gwas_qc/blacks/blacks_riva_ibd.genome")
 warf_ibd<-fread("/home/ashley/account/gwas_qc/blacks/blacks_warf_ibd.genome")
+
+#All Drugs IBD plot
+pdf("/home/ashley/account/gwas_qc/blacks/all_drugs_ibd.pdf")
+ggplot(data=all_ibd, aes(x=Z0, y=Z1)) +
+    geom_point(alpha=1/4) +
+    theme_bw() +
+    coord_cartesian(xlim =c(0, 1), ylim = c(0, 1)) +
+    theme(text = element_text(size = 12), plot.title = element_text(hjust = 0.5)) +
+    ggtitle("Blacks All Drugs IBD")
+dev.off()
+
 
 #Clop IBD plot
 pdf("/home/ashley/account/gwas_qc/blacks/clop_ibd.pdf")
