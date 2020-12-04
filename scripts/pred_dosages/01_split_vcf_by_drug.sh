b73eb32d13186f337b506a7568c2903bc2776a96
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-12-04 14:46:00 -0500

    Update 01_split_vcf_by_drug.sh

--- scripts/pred_dosages/01_split_vcf_by_drug.sh
+++ scripts/pred_dosages/01_split_vcf_by_drug.sh
@@ -3,8 +3,9 @@
 #remove some individuals based on IBD QC
 
 #VCFs have been imputed, filtered, and are separated by chr
-#Create 5 sets of VCFs, keep individuals in each corresponding drug group:
+#Create sets of VCFs, keep individuals in each corresponding drug group:
 
+#Example with Clop:
 for chr in {1..22};
 do vcftools --gzvcf /home/ashley/account/vcfs/blacks_imputed/chr${chr}_maf01_r28.vcf.gz --keep clop_ids_new_filt.txt --recode --out clop_chr${chr};
 done
