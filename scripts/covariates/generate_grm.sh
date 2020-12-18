ac4f7a67998736a33c02240fcc2fb47bd313050a
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-12-18 15:09:50 -0500

    Create generate_grm.sh

---
+++ scripts/covariates/generate_grm.sh
@@ -0,0 +1,18 @@
+#Create Relatedness Matrix with GEMMA - one per drug group
+#Use pre-imputation plinkfiles
+
+#Create bed/bim/fam for each drug based on indiividual list:
+
+#Example with clop:
+plink --bfile ACCOuNTBLACKSfinal_updated --keep /home/ashley/account/vcfs/blacks_imputed/clop/clop_ids.txt --make-bed --out pre_imputed_blacks_clop
+
+#Generate Relatedness Matrices:
+#Example with clop:
+gemma -bfile pre_imputed_blacks_clop -gk 1 -o blacks_clop
+
+#Generate Similarity/Relatedness Matirces:
+#Examples with clop:
+plink --bfile pre_imputed_blacks_clop --cluster --matrix --out blacks_clop
+
+
+#Note: I had issues running with gemma, did not recognize plinkfiles properly, I created matrices with plink instead
