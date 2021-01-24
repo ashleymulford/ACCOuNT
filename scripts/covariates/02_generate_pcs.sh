7646752d17309943b6a7ac83448c31b1873bc1f9
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2021-01-24 13:48:54 -0600

    Update and rename 02_generate_pcs.R to 02_generate_pcs.sh

---
+++ scripts/covariates/02_generate_pcs.sh
@@ -0,0 +1,20 @@
+#Principal Component Analysis with King:
+#Use pre-imputed plinkfiles, get PCs for each drug group
+
+#Example with clop:
+/home/ashley/king --pca -b pre_imputed_blacks_clop.bed --prefix blacks_clop_pca_
+
+#Note: currently having issue with riva and warf - getting 0.00 for eigen values and pcs
+#clop and apix output fine
+
+#With mds instead of pca:
+/home/ashley/king --mds -b pre_imputed_blacks_clop.bed --prefix blacks_clop_mds_
+
+#This works for all drugs - no 0 values
+
+#main diff between output of --pca and --mds:
+#--pca produces eigen values ~1500-900
+#--mds produces eigen values ~0.3-0.1
+#10 pcs in file output are of similar scale though (decimals between +1 and -1)
+
+
