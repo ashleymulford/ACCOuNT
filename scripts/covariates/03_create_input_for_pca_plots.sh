ecf20e8cd35afdc77411c1ef0ff5bcdf5dab0551
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2021-01-28 21:49:03 -0600

    Update 03_create_input_for_pca_plots.sh

--- scripts/covariates/03_create_input_for_pca_plots.sh
+++ scripts/covariates/03_create_input_for_pca_plots.sh
@@ -4,29 +4,18 @@
 
 #Must run pca with all pops, start by merging plinkfiles:
 
+#attempt merge - fails, use missnp output to exclude bad snps
 plink --bfile /home/ashley/account/vcfs/preimputation_plinkfiles/clop/missingness_hwe_steps/05filtered_HWE --bmerge /home/ashley/account/covariates/hapmap_pops_plinkfiles_for_pcs/hapmap.hg38 --make-bed --out pre_imputed_blacks_clop_hapmap_merged
 
+#exclude bad snps from both files
 plink --bfile hapmap.hg38 --exclude /home/ashley/account/vcfs/preimputation_plinkfiles/clop/pre_imputed_blacks_clop_hapmap_merged-merge.missnp --make-bed --out hapmap.hg38_excluded_snps
 plink --bfile /home/ashley/account/vcfs/preimputation_plinkfiles/clop/missingness_hwe_steps/05filtered_HWE --exclude pre_imputed_blacks_clop_hapmap_merged-merge.missnp --make-bed --out pre_imputed_blacks_clop_excluded_snps
 
+#merge new files
 plink --bfile pre_imputed_blacks_clop_excluded_snps --bmerge /home/ashley/account/covariates/hapmap_pops_plinkfiles_for_pcs/hapmap.hg38_excluded_snps --make-bed --out pre_imputed_blacks_clop_hapmap_excluded_snps_merged
 
-
-
-
-#Tried to run merge command but gave an error about number of alleles in certain SNPs (output missnp file with ~300 SNPs)
-plink --bfile pre_imputed_blacks_clop --bmerge /home/ashley/account/covariates/hapmap_pops_plinkfiles_for_pcs/hapmap.hg38 --make-bed --out pre_imputed_blacks_clop_hapmap_merged
-
-#Removed problematic SNPs from hapmap file
-plink --bfile hapmap.hg38 --exclude /home/ashley/account/vcfs/preimputation_plinkfiles/clop/pre_imputed_blacks_clop_hapmap_merged-merge.missnp --make-bed --out hapmap.hg38_excluded_snps
-
-#Ran merge again, worked this time
-plink --bfile pre_imputed_blacks_clop --bmerge /home/ashley/account/covariates/hapmap_pops_plinkfiles_for_pcs/hapmap.hg38_excluded_snps --make-bed --out pre_imputed_blacks_clop_hapmap_merged
-
-
 #Run PCA with merged plinkfiles:
-/home/ashley/king --mds -b pre_imputed_blacks_clop_hapmap_merged.bed --prefix blacks_clop_hapmap_merged_mds_
+/home/ashley/king --pca -b pre_imputed_blacks_clop_hapmap_excluded_snps_merged.bed --prefix blacks_clop_hapmap_merged_
 
 #Largest 10 Eigenvalues:
-#57.69 18.20 12.76 2.39 0.37 0.37 0.37 0.37 0.37 0.37
-
+#3569.71 2389.00 646.99 590.33 494.22 488.86 487.72 486.80 485.43 485.22
