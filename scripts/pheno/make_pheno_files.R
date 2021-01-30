cee69d54fb74252415df9f533e80933f9fb4d50b
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2021-01-29 21:02:49 -0600

    Update make_pheno_files.R

--- scripts/pheno/make_pheno_files.R
+++ scripts/pheno/make_pheno_files.R
@@ -5,6 +5,7 @@ clop<-fread("account_clopidogrel_processed_matched_IDs_all.csv")
 noac<-fread("account_noac_processed_matched_IDs_all.csv")
 warf<-fread("account_warfarin_processed_matched_IDs_all.csv")
 
+#save ids, pheno, and covariate columns only, remove unneeded columns so files are easier to work with
 clop_small<-clop[, c(1:9,28,203)]
 noac_small<-noac[, c(1:9,21,22,79,83)]
 #idk which pheno for warf, add later
@@ -14,6 +15,8 @@ fwrite(clop_small, "clop_pheno_with_ids.txt", sep = "\t", quote = F, na = NA)
 fwrite(noac_small, "noac_pheno_with_ids.txt", sep = "\t", quote = F, na = NA)
 #fwrite(warf_small, "warf_pheno_with_ids.txt", sep = "\t", quote = F, na = NA)
 
+
+##single column pheno files are made in script that make covariates: see https://github.com/ashleymulford/ACCOuNT/blob/master/scripts/covariates/05_make_covariate_files.R
 
 
 
