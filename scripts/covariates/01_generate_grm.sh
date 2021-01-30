7361958aae61716a36d1f98065fb9ad421ea7279
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2021-01-29 20:19:06 -0600

    Update 01_generate_grm.sh

--- scripts/covariates/01_generate_grm.sh
+++ scripts/covariates/01_generate_grm.sh
@@ -7,12 +7,13 @@
 plink --bfile /home/ashley/account/vcfs/preimputation_plinkfiles/ACCOuNTBLACKSfinal_updated --keep /home/ashley/account/vcfs/blacks_imputed/clop/clop_ids_filt.txt --make-bed --out pre_imputed_blacks_clop
 
 #Generate Relatedness Matrices:
+#use excluded_snps file, generated when plotting pcs
+#change all phenos from missing (-9) to 1 in fam file (find/replace with nano)
 #Example with clop:
-gemma -bfile pre_imputed_blacks_clop -gk 1 -o blacks_clop
+gemma -bfile pre_imputed_blacks_clop_excluded_snps -gk 1 -o blacks_clop
 
 #Generate Similarity/Relatedness Matirces:
 #Examples with clop:
 plink --bfile pre_imputed_blacks_clop --cluster --matrix --out blacks_clop
 
 
-#Note: I had issues running with gemma, did not recognize plinkfiles properly, I created matrices with plink instead
