c90f71ec52bca3e83e5d3f4eadf0dccc8f1e846b
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2021-02-02 17:01:44 -0600

    Update 01_predict_proteome.sh

--- scripts/pwas/01_predict_proteome.sh
+++ scripts/pwas/01_predict_proteome.sh
@@ -1,15 +1,17 @@
-#both pops:
+#Run PrediXcan with all drug groups and protein models:
 
+declare -a drugs=("clop" "apix" "riva" "warf")
+declare -a model_pops=("AFA" "ALL" "CAU" "CHN" "HIS")
 
-python3 /home/wheelerlab3/MetaXcan/software/Predict.py \
---model_db_path /home/ryan/topmed/proteome/dapg_net_CORRECT_WINDOWS/current_dbs/PAV_filteredALL_WG_baseline_rho0.1_zpval0.05.db \
---text_genotypes /home/ashley/account/dosages/blacks/pred_dosages/chr*.maf0.01.r20.8.dosage.txt.gz \
---text_sample_ids /home/ashley/account/dosages/blacks/samples_blacks.txt \
---prediction_output /home/ashley/account/pwas_results/blacks/predicted_levels/blacks_PAV_filteredALL_WG_baseline_rho0.1_zpval0.05.txt
+for model_pop in ${model_pops[@]}
+  do
+  for drug in ${drugs[@]}
+    do
+    python3 /home/wheelerlab3/MetaXcan/software/Predict.py \
+    --model_db_path /home/ryan/TOPMed_Proteome/dbs_out/${model_pop}_PCAIR_baseline_models_PAV_filtered_rho0.1_zpval0.05.db \
+    --text_genotypes /home/ashley/account/dosages/blacks/${drug}/pred_dosages/${drug}_chr*.dosage.txt.gz \
+    --text_sample_ids /home/ashley/account/dosages/blacks/${drug}/samples_${drug}.txt \
+    --prediction_output /home/ashley/account/pwas_results/blacks/${drug}/predicted_levels/blacks_${drug}_PCAIR_PAV_filtered_${model_pop}_WG_baseline_rho0.1_zpval0.05.txt
+  done
+done
 
-
-python3 /home/wheelerlab3/MetaXcan/software/Predict.py \
---model_db_path /home/ryan/topmed/proteome/dapg_net_CORRECT_WINDOWS/current_dbs/PAV_filteredALL_WG_baseline_rho0.1_zpval0.05.db \
---text_genotypes /home/ashley/account/dosages/whites/pred_dosages/chr*.maf0.01.r20.8.dosage.txt.gz \
---text_sample_ids /home/ashley/account/dosages/whites/samples_whites.txt \
---prediction_output /home/ashley/account/pwas_results/whites/predicted_levels/whites_PAV_filteredALL_WG_baseline_rho0.1_zpval0.05.txt
