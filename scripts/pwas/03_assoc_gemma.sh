1954b061b1aac80f415a4a1a2ba1a83ba4ae9faf
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2021-02-02 17:36:37 -0600

    Update 03_assoc_gemma.sh

--- scripts/pwas/03_assoc_gemma.sh
+++ scripts/pwas/03_assoc_gemma.sh
@@ -1,6 +1,6 @@
+#Association Studies with GEMMA for all predicted levels for all drug groups
 
-
-declare -a drugs=("arac" "cape" "carbo" "cis" "dauno" "etop" "pacl" "peme")
+declare -a drugs=("clop" "apix" "riva")
 declare -a models=("CAU" "ALL" "AFA" "CHN" "HIS")
 
 #Use loop to run PrediXcan step two through GEMMA (accounts for relatedness)
@@ -8,6 +8,12 @@ for drug in ${drugs[@]}
   do
   for model in ${models[@]}
     do
-    /usr/local/bin/gemma -g /home/ashley/LCL_chemotherapy/YRI/YRI_pwas_results/association_input/YRI_PCAIR_PAV_filtered_${model}_baseline_rho0.1_zpval0.05_gemma.txt -notsnp -p /home/ashley/LCL_chemotherapy/YRI/YRI_phenotypes/YRI_${drug}_bestpheno_noids.txt -k /home/ashley/LCL_chemotherapy/YRI/YRI_relationship_matrix_maf.cXX.txt -lmm 4 -o YRI_${drug}_PCAIR_PAV_filtered_${model}_baseline_rho0.1_zpval0.05
+    /usr/local/bin/gemma -g /home/ashley/account/pwas_results/blacks/${drug}/assoc_input_gemma/blacks_${drug}_PCAIR_PAV_filtered_${model}_baseline_rho0.1_zpval0.05_gemma.txt \
+    -notsnp \
+    -p /home/ashley/account/phenos/${drug}/${drug}_pheno_noids.txt \
+    -k /home/ashley/account/covariates/${drug}/blacks_${drug}.cXX.txt \
+    -c /home/ashley/account/covariates/${drug}/${drug}_covariates.txt \
+    -lmm 4 \
+    -o ${drug}_PCAIR_PAV_filtered_${model}_baseline_rho0.1_zpval0.05
   done
 done
