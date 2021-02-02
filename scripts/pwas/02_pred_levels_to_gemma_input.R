da4549eaf041fd60aa79e5ebb51282b6533df910
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2021-02-02 17:09:09 -0600

    Update 02_pred_levels_to_gemma_input.R

--- scripts/pwas/02_pred_levels_to_gemma_input.R
+++ scripts/pwas/02_pred_levels_to_gemma_input.R
@@ -10,7 +10,7 @@ drug_list <- c("clop", "apix", "riva", "warf")
 
 for(model in model_list){
   for(drug in drug_list){
-    pred_exp <- fread("/home/ashley/LCL_chemotherapy/YRI/YRI_pwas_results/predicted_levels/YRI_PCAIR_PAV_filtered_" %&% model %&% "_baseline_rho0.1_zpval0.05.txt", header = F) #path to PrediXcan-produced _predicted_expression.txt
+    pred_exp <- fread("/home/ashley/account/pwas_results/blacks/" %&% drug %&% "/predicted_levels/blacks_" %&% drug %&% "_PCAIR_PAV_filtered_" %&% model %&% "_baseline_rho0.1_zpval0.05.txt", header = F) #path to PrediXcan-produced _predicted_expression.txt
     pred_exp$V1 <- NULL
     pred_exp$V2 <- NULL
     pred_exp <- transpose(pred_exp)
@@ -21,6 +21,6 @@ for(model in model_list){
     pred_exp$V1 <- NA
     colnames(gene_list) <- "V"
     pred_exp <- cbind(gene_list, pred_exp)
-    fwrite(pred_exp, "/home/ashley/LCL_chemotherapy/YRI/YRI_pwas_results/association_input/YRI_PCAIR_PAV_filtered_" %&% model %&% "_baseline_rho0.1_zpval0.05_gemma.txt", na = "NA", quote = F, sep = "\t", col.names = F) #path to GEMMA input/pseudo-genotype
+    fwrite(pred_exp, "/home/ashley/account/pwas_results/blacks/" %&% drug %&% "/assoc_gemma_input/blacks_" %&% drug %&% "_PCAIR_PAV_filtered_" %&% model %&% "_baseline_rho0.1_zpval0.05_gemma.txt", na = "NA", quote = F, sep = "\t", col.names = F) #path to GEMMA input/pseudo-genotype
   }
 }
