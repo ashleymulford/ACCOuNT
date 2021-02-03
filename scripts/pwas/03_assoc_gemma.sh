7ded290129269c88e6d5314eed83af08c4a78af0
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2021-02-03 12:05:59 -0600

    Update 03_assoc_gemma.sh

--- scripts/pwas/03_assoc_gemma.sh
+++ scripts/pwas/03_assoc_gemma.sh
@@ -10,7 +10,7 @@ for drug in ${drugs[@]}
     do
     /usr/local/bin/gemma -g /home/ashley/account/pwas_results/blacks/${drug}/assoc_input_gemma/blacks_${drug}_PCAIR_PAV_filtered_${model}_baseline_rho0.1_zpval0.05_gemma.txt \
     -notsnp \
-    -p /home/ashley/account/phenos/${drug}/${drug}_pheno_noids.txt \
+    -p /home/ashley/account/phenos/${drug}/${drug}_pheno_no_ids.txt \
     -k /home/ashley/account/covariates/${drug}/blacks_${drug}.cXX.txt \
     -c /home/ashley/account/covariates/${drug}/${drug}_covariates.txt \
     -lmm 4 \
