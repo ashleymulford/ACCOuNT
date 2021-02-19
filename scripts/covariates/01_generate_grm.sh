c507461bb25b4bf857c87d912131e4f3712532b9
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2021-02-19 14:34:23 -0600

    Update 01_generate_grm.sh

--- scripts/covariates/01_generate_grm.sh
+++ scripts/covariates/01_generate_grm.sh
@@ -7,14 +7,15 @@
 plink --bfile /home/ashley/account/vcfs/preimputation_plinkfiles/ACCOuNTBLACKSfinal_updated --keep /home/ashley/account/vcfs/blacks_imputed/clop/clop_ids_filt.txt --make-bed --out pre_imputed_blacks_clop
 
 #Generate Relatedness Matrices:
-#use excluded_snps file, generated when plotting pcs
+#use excluded_snps file or 05filtered_HWE file, generated when making/plotting pcs - see scripts 02 and 03
 #change all phenos from missing (-9) to 1 in fam file (find/replace with nano)
 #Example with clop:
 gemma -bfile pre_imputed_blacks_clop_excluded_snps -gk 1 -o blacks_clop
+#Example with noac:
+gemma -bfile /home/ashley/account/vcfs/preimputation_plinkfiles/noac/missingness_hwe_steps/05filtered_HWE -gk 1 -o blacks_noac
+
 
 #Generate Similarity/Relatedness Matirces:
 #Examples with clop:
 plink --bfile pre_imputed_blacks_clop --cluster --matrix --out blacks_clop
 
-
-gemma -bfile pre_imputed_blacks_noac_excluded_snps -gk 1 -o blacks_noac
