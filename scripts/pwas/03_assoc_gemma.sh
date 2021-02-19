1d26092458139580ad24000e4616817bc02f9c4d
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2021-02-19 17:19:53 -0600

    Update 03_assoc_gemma.sh

--- scripts/pwas/03_assoc_gemma.sh
+++ scripts/pwas/03_assoc_gemma.sh
@@ -39,6 +39,23 @@ for drug in ${drugs[@]}
   done
 done
 
+#NOAC
+declare -a drugs=("noac")
+declare -a models=("ALL" "AFA")
 
+#Use loop to run PrediXcan step two through GEMMA (accounts for relatedness)
+for drug in ${drugs[@]}
+  do
+  for model in ${models[@]}
+    do
+    /usr/local/bin/gemma -g /home/ashley/account/pwas_results/blacks/${drug}/assoc_gemma_input/blacks_${drug}_PCAIR_PAV_filtered_${model}_baseline_rho0.1_zpval0.05_gemma.txt \
+    -notsnp \
+    -p /home/ashley/account/phenos/${drug}/${drug}_rn_pheno_no_ids.txt \
+    -k /home/ashley/account/covariates/${drug}/blacks_${drug}.cXX.txt \
+    -c /home/ashley/account/covariates/${drug}/${drug}_covariates.txt \
+    -lmm 4 \
+    -o ${drug}_rn_PCAIR_PAV_filtered_${model}_baseline_rho0.1_zpval0.05
+  done
+done
 
 
