8d0a1b9879c2bd26d0ba48065da2c39b443e100d
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2021-03-22 23:22:35 -0500

    Update 01_predict_transcipt_levels.sh

--- scripts/twas/01_predict_transcipt_levels.sh
+++ scripts/twas/01_predict_transcipt_levels.sh
@@ -1,5 +1,7 @@
 #TWAS MESA models are in build 37 - must liftover dosages to build 37 from build 38 first - see liftover folder for scripts
 
+#(re-)gzip all dosage files after liftover (both b37 and b38)
+
 #Predict transcript levels for four drug groups using AFA and ALL MESA models
 
 declare -a drugs=("apix" "clop" "riva" "noac")
@@ -11,7 +13,7 @@ for model_pop in ${model_pops[@]}
     do
     python3 /home/wheelerlab3/MetaXcan/software/Predict.py \
     --model_db_path /home/ashley/MESA_dbs/${model_pop}_imputed_10_peer_3_pcs_v2.db \
-    --text_genotypes /home/ashley/account/dosages/blacks/${drug}/pred_dosages/${drug}_chr*.dosage.txt.gz \
+    --text_genotypes /home/ashley/account/liftover/lifted_dosages_b37/${drug}/b37_dosages/${drug}_chr*_b37_ids.dosage.txt.gz \
     --text_sample_ids /home/ashley/account/dosages/blacks/${drug}/samples_${drug}.txt \
     --prediction_output /home/ashley/account/twas_results/${drug}/predicted_levels/blacks_${drug}_${model_pop}_filtered_predicted_transcript.txt
   done
