c5c8f162274f54ac6d17907ae30c1ca4821084b4
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2021-03-22 22:07:33 -0500

    Create 01_generate_lifted_files.sh

---
+++ scripts/twas/liftover/01_generate_lifted_files.sh
@@ -0,0 +1,12 @@
+#After downloading liftover file (hg38ToHg19.over.chain.gz)
+
+#generate lifted file for all dosages, split by chr:
+
+declare -a drugs=("apix" "clop" "riva" "noac")
+for drug in ${drugs[@]}
+  do
+  for chr in {1..22}
+    do awk '{print $1 FS $3 FS $3+1 FS $2}' /home/ashley/account/dosages/blacks/${drug}/pred_dosages/${drug}_chr${chr}.dosage.txt.gz > /home/ashley/account/liftover/lifted_dosages_b37/${drug}/prelifted_files/${drug}_chr${chr}_prelift.txt
+    liftOver /home/ashley/account/liftover/lifted_dosages_b37/${drug}/prelifted_files/${drug}_chr${chr}_prelift.txt /home/ashley/LCL_chemotherapy/hg19ToHg38.over.chain.gz /home/ashley/account/liftover/lifted_dosages_b37/${drug}/lifted_files/${drug}_chr${chr}_lifted.txt /home/ashley/account/liftover/lifted_dosages_b37/${drug}/lifted_files/${drug}_chr${chr}_unmapped.txt;
+  done
+done
