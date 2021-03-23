43d08ad58c2ed6210849bc6b1b20ed3a1692d085
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2021-03-22 22:37:44 -0500

    Update 01_generate_lifted_files.sh

--- scripts/twas/liftover/01_generate_lifted_files.sh
+++ scripts/twas/liftover/01_generate_lifted_files.sh
@@ -1,6 +1,7 @@
 #After downloading liftover file (hg38ToHg19.over.chain.gz)
 
-#generate lifted file for all dosages, split by chr:
+#generate lifted file for all dosages, split by chr for each drug:
+#converting from b38 to b37
 
 declare -a drugs=("apix" "clop" "riva" "noac")
 for drug in ${drugs[@]}
@@ -10,3 +11,8 @@ for drug in ${drugs[@]}
     liftOver /home/ashley/account/liftover/lifted_dosages_b37/${drug}/prelifted_files/${drug}_chr${chr}_prelift.txt /home/ashley/LCL_chemotherapy/hg19ToHg38.over.chain.gz /home/ashley/account/liftover/lifted_dosages_b37/${drug}/lifted_files/${drug}_chr${chr}_lifted.txt /home/ashley/account/liftover/lifted_dosages_b37/${drug}/lifted_files/${drug}_chr${chr}_unmapped.txt;
   done
 done
+
+
+#dosage files must be unzipped for use
+
+#next step: use lifted files to generate new dosage files in b37
