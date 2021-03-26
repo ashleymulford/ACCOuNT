7d3359d2914e259e5d19d8d6093ff61bea899703
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2021-03-26 14:49:59 -0500

    Update 04_split_vcf_anno_by_chr.sh

--- scripts/twas/liftover/04_split_vcf_anno_by_chr.sh
+++ scripts/twas/liftover/04_split_vcf_anno_by_chr.sh
@@ -1,1 +1,5 @@
 # Split VCF anno by chr for use with indvidual dosages when getting b37 rsids
+
+for chr in {1..22};
+do zgrep NC_00000${chr} GCF_000001405.25.gz > /home/ashley/account/liftover/rsid_anno/anno_chr${chr}_vcf.txt;
+done
