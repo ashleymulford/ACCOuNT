1f59162d88df36c356eb3ca18bfc01f9fc9ade7a
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2021-03-26 17:57:20 -0500

    Update 01_predict_transcipt_levels.sh

--- scripts/twas/01_predict_transcipt_levels.sh
+++ scripts/twas/01_predict_transcipt_levels.sh
@@ -1,10 +1,8 @@
-#TWAS MESA models are in build 37 - must liftover dosages to build 37 from build 38 first - see liftover folder for scripts
+#TWAS MESA models are in build 37 - must liftover dosages to build 37 from build 38 first and then map to rsids - see liftover folder for scripts
 
 #(re-)gzip all dosage files after liftover (both b37 and b38)
 
 #Predict transcript levels for four drug groups using AFA and ALL MESA models
-
-#Currently getting 0% snps in models after liftover - prob issue with ids, may need rsids instead of chr:pos ids
 
 declare -a drugs=("apix" "clop" "riva" "noac")
 declare -a model_pops=("AFA" "ALL")
@@ -15,8 +13,27 @@ for model_pop in ${model_pops[@]}
     do
     python3 /home/wheelerlab3/MetaXcan/software/Predict.py \
     --model_db_path /home/ashley/MESA_dbs/${model_pop}_imputed_10_peer_3_pcs_v2.db \
-    --text_genotypes /home/ashley/account/liftover/lifted_dosages_b37/${drug}/b37_dosages/${drug}_chr*_b37_ids.dosage.txt.gz \
+    --text_genotypes /home/ashley/account/liftover/lifted_dosages_b37/${drug}/b37_dosages/${drug}_chr*_rsid_b37.dosage.txt.gz \
     --text_sample_ids /home/ashley/account/dosages/blacks/${drug}/samples_${drug}.txt \
     --prediction_output /home/ashley/account/twas_results/${drug}/predicted_levels/blacks_${drug}_${model_pop}_filtered_predicted_transcript.txt
+  done
+done
+
+
+
+#Predict transcript levels for four drug groups using relavent mashr models
+
+declare -a drugs=("apix" "clop" "riva" "noac")
+declare -a model_tiss=("Whole_Blood" "Heart_Atrial_Appendage" "Heart_Left_Ventricle" "Liver")
+
+for tiss in ${model_tiss[@]}
+  do
+  for drug in ${drugs[@]}
+    do
+    python3 /home/wheelerlab3/MetaXcan/software/Predict.py \
+    --model_db_path /home/ashley/gtex_v8_mashr_eqtl_dbs/mashr_${tiss}.db \
+    --text_genotypes /home/ashley/account/liftover/lifted_dosages_b37/${drug}/b37_dosages/${drug}_chr*_rsid_b37.dosage.txt.gz \
+    --text_sample_ids /home/ashley/account/dosages/blacks/${drug}/samples_${drug}.txt \
+    --prediction_output /home/ashley/account/twas_results/${drug}/predicted_levels/blacks_${drug}_${tiss}_filtered_predicted_transcript.txt
   done
 done
