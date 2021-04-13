9a2c4a9b0d6c81c55e048d78ab47b3765531b9a5
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2021-04-13 14:05:34 -0500

    Update 01_predict_proteome.sh

--- scripts/pwas/01_predict_proteome.sh
+++ scripts/pwas/01_predict_proteome.sh
@@ -31,3 +31,26 @@ for model_pop in ${model_pops[@]}
     --prediction_output /home/ashley/account/pwas_results/blacks/${drug}/predicted_levels/blacks_${drug}_PCAIR_PAV_filtered_${model_pop}_baseline_rho0.1_zpval0.05.txt
   done
 done
+
+
+#Run PrediXcan with all drug groups and dap-g protein models:
+
+declare -a drugs=("clop" "apix" "riva" "noac")
+declare -a model_pops=("AFA" "ALL")
+
+for model_pop in ${model_pops[@]}
+  do
+  for drug in ${drugs[@]}
+    do
+    python3 /home/wheelerlab3/MetaXcan/software/Predict.py \
+    --model_db_path /home/ryan/topmed/proteome/current_dbs/${model_pop}_PCAIR_dapg_0.001_T_PAV_filtered_rho0.1_zpval0.05.db \
+    --text_genotypes /home/ashley/account/dosages/blacks/${drug}/pred_dosages/${drug}_chr*.dosage.txt.gz \
+    --text_sample_ids /home/ashley/account/dosages/blacks/${drug}/samples_${drug}.txt \
+    --prediction_output /home/ashley/account/pwas_results/blacks/${drug}/predicted_levels/blacks_${drug}_PCAIR_PAV_filtered_dapg_0.001_T_${model_pop}_baseline_rho0.1_zpval0.05.txt
+  done
+done
+
+
+
+
+
